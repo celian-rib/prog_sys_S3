d2ce0e750fd21e94f0f2adb4c23032c6be89072b
celian <celian.riboulet@gmail.com>
2022-01-10 21:41:00 +0100

    Read & Write cleanup

--- 01_write_read/main.c
+++ 01_write_read/main.c
@@ -2,57 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// int main() {
-//     char tampon [ ] = "Hello, world 123\n";
-//     write(STDOUT_FILENO, tampon, 17);
-//     return 0;
-// }
-
-// int main() {
-//     write(STDOUT_FILENO, "Ecrire : ", 10);
-//     char tampon[100];
-//     int nb = read(STDIN_FILENO, tampon, 100);
-//     write(STDOUT_FILENO, "J'ai lu : ", 10);
-//     write(STDOUT_FILENO, tampon, nb);
-// }
-
-// int main() {
-//     chdir("/bin");
-//     system("ls");
-// }
-
-// typedef void (*une_fct)();
-
-// void manger() {
-//     printf("Je mange\n");
-// }
-
-// void dormir(int heures) {
-//     printf("Je dors %d heures\n", heures);
-// }
-
-// int main() {
-//     une_fct a = manger;
-//     a(); // Appel à manger()
-//     a = dormir;
-//     a(8); // Appel à dormir avec un paramètre
-// }
-
-// int main() {
-//     pid_t p = fork();
-//     if (p == 0)
-//         exit(EXIT_FAILURE);
-//     int status;
-//     waitpid(p, &status, 0);
-//     printf("le s'est terminé avec le status %d\n", status);
-// }
-
-// int main() {
-//     execl ("/bin/ls", "ls", "-la", NULL);
-// }
-
 int main() {
-    for(int i = 0; i < 2; i++)
-        fork();
-    printf("pid = %d\n", getpid());
+    // Ecrire dans le fichier STDOUT_FILENO (Soit la sortie standard = le terminal)
+    write(STDOUT_FILENO, "Ecrire : ", 10);
+
+    // Lecture de l'entrée standard
+    char tampon[100]; // Variable dans laquelle on va mettre ce qu'on a lu
+    int nb = read(STDIN_FILENO, tampon, 100); // Lire un fichier (Ici STDIN_FILENO soit l'entrée standard)
+
+    // On écrit le résultat :
+    write(STDOUT_FILENO, "J'ai lu : ", 10);
+    write(STDOUT_FILENO, tampon, nb);
 }
